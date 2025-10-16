54d2a35c74be68da6fd60b25ff0542e701f4980e
unknown <asolleo287@g.educaand.es>
2025-10-16 14:16:16 +0200

    16/10

--- js/myscript.js
+++ js/myscript.js
@@ -153,5 +153,21 @@ function myAlert(msg) {
     var div = document.createElement("div");
     div.classList.add("alert");
     div.innerHTML = msg;
+    var close = document.createElement("span");
+    close.style.float = "right";
+    close.classList.add("close");
+    close.innerHTML = "X";
+    div.appendChild(close);
     $("body").insertBefore(div,$("body").firstChild);
+    bind_close();
+}
+
+function bind_close(){
+    let elements = document.querySelectorAll(".close");
+    for (var i=elements.length-1;i>=0;i--) {
+        let el = elements[i];
+        el.addEventListener("click", function(){
+            this.parentNode.style.display = "none";
+        });
+    }
 }
